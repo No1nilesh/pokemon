af13edd1bdb5bf45e483f3d20d581e2ee0e4f49b
Nilesh Gautam <gautamnilesh084@gmail.com>
2025-05-03 20:43:31 +0530

    added option to route through evoution pokemons

--- pokemon/src/pages/Pokemon/Evolution.jsx
+++ pokemon/src/pages/Pokemon/Evolution.jsx
@@ -5,12 +5,13 @@ import { getPokemonData } from "../../app/reducer/getPokemonData";
 import Loader from "../../components/Loader.jsx";
 import { getInitialPokemonData } from "../../app/reducer/getInitialPokemonData.js";
 import { findEvolution } from "../../../utils/findEvolution.js";
+import { useNavigate } from "react-router-dom";
+import { setPokemonTab } from "../../app/slice/tabSlice.js";
 
 function Evolution({ evolution }) {
   const { allPokemon, randomPokemon, loading } = useSelector((state) => state.pokemon);
   const dispatch = useDispatch();
-
-
+  const navigate = useNavigate();
   useEffect(() => {
     dispatch(getInitialPokemonData())
   }, [dispatch])
@@ -25,6 +26,10 @@ function Evolution({ evolution }) {
     }
   }, [allPokemon, evolution, dispatch]);
 
+  const handleEvolutionClick=(pokemon)=>{
+    navigate(`/pokemon/${pokemon.id}`)
+    dispatch(setPokemonTab('description'))
+  }
 
 
   if (loading) {
@@ -42,11 +47,11 @@ function Evolution({ evolution }) {
         </div>
       </div>
 
-      <div className="evolution-list mt-4 flex flex-col md:flex-row w-full justify-evenly items-center flex-1 mb-20 md:mb-0">
+      <div className="evolution-list mt-4 flex flex-col md:flex-row w-full justify-evenly items-center flex-1 mb-20 md:mb-0 overflow-x-auto">
         {
           randomPokemon.map((pokemon) => {
             return (
-              <div key={pokemon.id} className="flex flex-col" >
+              <div onClick={()=> handleEvolutionClick(pokemon)} key={pokemon.id} className="flex flex-col" >
                 <div className="md:ml-20 rounded-sm bg-primary-card/60 h-fit w-48 px-4 py-4 text-left relative before:content[''] before:rounded-tl-sm before:h-1 before:w-16 before:bg-card-border before:absolute before:top-0 before:left-0">
                   <div className="text-card uppercase text-xl font-semibold ">
                     <span>{pokemon.name}</span>
